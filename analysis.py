a3ffe07de7ed487b1ecc90cfec539419816c3dd6
unknown <G00398269@gmit.ie>
2021-04-13 00:35:19 +0100

    analysis.py modified: summary_variables function added

--- analysis.py
+++ analysis.py
@@ -14,3 +14,26 @@ def read_iris_dataset():
     return iris_dataframe
     
 iris_dataframe = read_iris_dataset()        
+
+
+def summary_variables():
+    
+    shape = (len(iris_dataframe.axes[1]),len(iris_dataframe.axes[0]))
+    data_types = iris_dataframe.dtypes                                                            
+    species_count = iris_dataframe.groupby('species').size()
+    null_count = iris_dataframe.isnull().sum()   # True for NaN / blank values
+    desc_all_species = iris_dataframe.describe()
+    correlation = iris_dataframe.corr()
+    pd.set_option("display.precision", 2)
+
+    # Check if numeric columns can be converted to integer
+    numeric_columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+    try:
+        iris_dataframe[numeric_columns].astype(float).astype(int)
+        integer_check = 'The following columns only contain numeric values:\n{}.'.format(numeric_columns)      
+    except ValueError:
+        integer_check = 'Not all entires in the following columns contain numeric values \n{}.'.format(numeric_columns)  
+    
+    return shape, data_types, null_count,species_count, integer_check, desc_all_species, correlation
+    
+summary_tuple = summary_variables()   
